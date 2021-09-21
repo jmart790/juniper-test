e7fde406b50bd67c2d68418d512247913526a9b1
Josh <jmart790@fiu.edu>
2021-09-21 16:09:48 -0400

    ce=reated radio group component

---
+++ src/components/molecules/RadioFilterGroup.vue
@@ -0,0 +1,52 @@
+<script>
+import RadioFilterBtn from "@/components/atoms/RadioFilterBtn.vue";
+
+export default {
+  name: "RadioFilterGroup",
+  components: { RadioFilterBtn },
+  model: {
+    props: "selected",
+    event: "change"
+  },
+  props: {
+    label: {
+      type: String,
+      required: false
+    },
+    options: {
+      type: Array,
+      required: true
+    },
+    value: {
+      type: [String, Number, Object],
+      required: false
+    }
+  },
+  methods: {
+    updateValue(value) {
+      this.$emit("change", value);
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="radio-filter-group">
+    <RadioFilterBtn
+      v-for="(option, index) in options"
+      :key="`${option.label}-${index}`"
+      :label="option.label"
+      :value="option.value"
+      :selected="value"
+      @change="updateValue(option.value)"
+    />
+  </div>
+</template>
+
+
+<style lang="scss">
+.radio-filter-group {
+  display: flex;
+  gap: $gap-4;
+}
+</style>
