36d17505bbd42a34317e53ad8037687ce776afa9
Josh <jmart790@fiu.edu>
2021-09-21 16:04:40 -0400

    set up initial lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,50 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+  extends: [
+    "plugin:vue/strongly-recommended", 
+    "eslint:recommended"
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    "parser": "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    "vue/attribute-hyphenation": 1,
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": 3,
+      "multiline": 1
+    }],
+    "vue/order-in-components": ["error", {
+      "order": [
+        "el", 
+        "name",
+        "key",
+        ["delimiters", "comments"],
+        "extends",
+        "mixins",
+        ["components", "directives", "filters"],
+        "validate",
+        "transition",
+        "loading",
+        "inheritAttrs",
+        "model",
+        ["props", "propsData"],
+        "emits",
+        "data",
+        "LIFECYCLE_HOOKS",
+        "watch",
+        "watchQuery",
+        "computed",
+        "methods",
+        "head",
+        ["template", "render"],
+        "renderError"
+      ]
+    }]
+  },
+};
