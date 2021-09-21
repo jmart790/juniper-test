9128f29197774b79c343b523e00caa8942b06052
Josh <jmart790@fiu.edu>
2021-09-21 16:09:28 -0400

    created star ratings component

---
+++ src/components/atoms/StarRatings.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+  name: "StarRatings",
+  props: {
+    rating: {
+      type: Number,
+      required: true
+    },
+    totalRatings: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
+
+<template>
+  <div
+    class="star-rating"
+    :style="{ '--rating': rating }"
+    :aria-label="
+      `This product is rated ${rating} out of 5 from ${totalRatings} reviews`
+    "
+  >
+    <div class="star-rating__wrapper">
+      <span
+        v-for="star in 5"
+        :key="`STAR-RATING-${star}`"
+        class="icon-ratings-solid-star star-rating__icon"
+      />
+    </div>
+    <span class="star-rating__total">( {{ totalRatings }} )</span>
+  </div>
+</template>
+
+
+<style lang="scss">
+// inspired by: https://codepen.io/FredGenkin/pen/eaXYGV
+.star-rating {
+  $percent: calc(var(--rating) / 5 * 100%);
+  display: flex;
+  align-items: center;
+  gap: $gap-4;
+  &__wrapper {
+    width: min-content;
+    display: flex;
+    gap: $gap-2;
+    background: linear-gradient(90deg, $apricot $percent, $grey-1 $percent);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  &__icon {
+    font-size: 14px;
+  }
+  &__total {
+    font-size: 12px;
+    font-weight: 600;
+    color: $grey-3;
+  }
+}
+</style>
