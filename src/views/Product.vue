b468070372c829f47b1c213da880eb0bce00f2e8
Josh <jmart790@fiu.edu>
2021-09-22 11:20:36 -0400

    created simple product page, in progress

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,15 +1,94 @@
 <script>
+import { mapGetters, mapActions } from "vuex";
+import StarRatings from "@/components/atoms/StarRatings";
+
 export default {
-  name: "Product"
+  name: "Product",
+  components: { StarRatings },
+  data() {
+    return {
+      imageQuery:
+        "?cropxunits=100&cropyunits=100&crop=2,2,98,98&w=300&h=300&mode=max"
+    };
+  },
+  async mounted() {
+    if (this.products.length === 0) {
+      await this.getStoreData();
+    }
+  },
+  computed: {
+    ...mapGetters(["products", "isLoading", "error"]),
+    product() {
+      const paramsId = this.$route.params.id;
+      if (paramsId && this.products.length > 0) {
+        return this.products.find(product => product.ProductID == paramsId);
+      }
+      return null;
+    }
+  },
+  methods: {
+    ...mapActions(["getStoreData"])
+  }
 };
 </script>
 
 <template>
-  <div>
-    <h1>Product</h1>
+  <div class="product">
+    <div class="product__header">
+      <h1>Product</h1>
+    </div>
+    <div v-if="product">
+      <img
+        :src="`${product.PhotoName}${imageQuery}`"
+        alt="Product image"
+        loading="lazy"
+      />
+
+      <div class="product__info">
+        <p class="product__name">
+          {{ product.ItemName }}
+        </p>
+        <StarRatings
+          class="product__rating"
+          :rating="product.rating"
+          :total-ratings="product.totalRatings"
+        />
+        <div class="product__details">
+          <label for="description">Description</label>
+          <p name="description">
+            {{ product.Description || "Product description coming soon..." }}
+          </p>
+          <label for="dimensions">Dimensions</label>
+          <p name="dimensions">
+            {{ product.Dimensions }}
+          </p>
+          <label for="item id">Item Id</label>
+          <p name="item id">
+            {{ product.ItemID }}
+          </p>
+        </div>
+      </div>
+
+      <p class="product__price" name="price">
+        <span class="icon-rewards-linear-dollar-sign" />
+        {{ product.BasePrice.toFixed(2) }}
+      </p>
+    </div>
   </div>
 </template>
 
 
 <style lang="scss">
+.product {
+  &__header {
+    margin-bottom: $gap-12;
+    display: flex;
+    flex-wrap: wrap;
+    gap: $gap-4;
+    @media screen and (min-width: $tablet) {
+      justify-content: space-between;
+      align-items: flex-end;
+    }
+  }
+}
 </style>
