dadcf634eba8439ef77f4db841e0956346b2f8a3
Josh <jmart790@fiu.edu>
2021-09-21 21:25:09 -0400

    created simple sales rep and product page

---
+++ src/views/Product.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "Product"
+};
+</script>
+
+<template>
+  <div>
+    <h1>Product</h1>
+  </div>
+</template>
+
+
+<style lang="scss">
+</style>
---
+++ src/views/SalesRep.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "SalesRep"
+};
+</script>
+
+<template>
+  <div>
+    <h1>Sales Rep</h1>
+  </div>
+</template>
+
+
+<style lang="scss">
+</style>
